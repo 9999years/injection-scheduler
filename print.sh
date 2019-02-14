4691551154e16fc513e86cea950c1da777636ebd
Rebecca Turner <637275@gmail.com>
2019-02-13 22:01:22 -0500

    print script

--- print.sh
+++ print.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH="/usr/sbin:/sbin:/home/pi/.local/bin:/usr/local/bin:/usr/bin:/bin"
+
 python3.7 injection_scheduler.py \
        | fmt -w 40 \
        | lp -o cpi=15 -o lpi=8
